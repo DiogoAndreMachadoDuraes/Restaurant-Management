103cc86caeb65b8e6ac31da73362e28fbceedf51
Diogo Durães <61517303+DiogoAndreMachadoDuraes@users.noreply.github.com>
2020-04-22 17:50:48 +0100

    cliente modify

--- app/models/Cliente.php
+++ app/models/Cliente.php
@@ -4,13 +4,19 @@ namespace App\Models;
 
     final class Cliente
     {
+        private int $id_cliente;
         private int $nif_cliente;
-        private string $nome;
-        private string $telefone;
-        private string $email;
-        private string $morada;
 
         public function Cliente(){ }
+
+        public function getId_cliente(){
+            return $this->id_cliente;
+        }
+
+        public function setId_cliente($id_cliente): Cliente{
+            $this->id_cliente = $id_cliente;
+            return $this;
+        }
 
         public function getNif(){
             return $this->nif_cliente;
@@ -18,42 +24,6 @@ namespace App\Models;
 
         public function setNif($nif_cliente): Cliente{
             $this->nif = $nif_cliente;
-            return $this;
-        }
-
-        public function getNome(){
-            return $this->nome;
-        }
-
-        public function setNome($nome): Cliente{
-            $this->nome = $nome;
-            return $this;
-        }
-
-        public function getTelefone(){
-            return $this->telefone;
-        }
-
-        public function setTelefone($telefone): Cliente{
-            $this->telefone = $telefone;
-            return $this;
-        }
-
-        public function getEmail(){
-            return $this->email;
-        }
-
-        public function setEmail($email): Cliente{
-            $this->email = $email;
-            return $this;
-        }
-
-        public function getMorada(){
-            return $this->morada;
-        }
-
-        public function setMorada($morada): Cliente{
-            $this->morada = $morada;
             return $this;
         }
     }
