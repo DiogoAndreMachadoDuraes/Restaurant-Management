860838615dba653c7af0ed8498954e278ce66cd1
Diogo Durães <61517303+DiogoAndreMachadoDuraes@users.noreply.github.com>
2020-04-23 15:42:35 +0100

    Alteração da base de dados local para base de dados do servidor

--- senderrors.php
+++ senderrors.php
@@ -1,9 +1,9 @@
 <?php
 
-    putenv('Ementas_Restauracao_HOST=localhost');
-    putenv('Ementas_Restauracao_DBNAME=Ementas_Restauracao');
-    putenv('Ementas_Restauracao_USER=root');
-    putenv('Ementas_Restauracao_PASSWORD=root');
+    putenv('Ementas_Restauracao_HOST=http://178.62.212.71/phpmyadmin/');
+    putenv('Ementas_Restauracao_DBNAME=ementasrestauracao');
+    putenv('Ementas_Restauracao_USER=rh_user');
+    putenv('Ementas_Restauracao_PASSWORD=rFrH2016');
 
     putenv('JWT_SECRET_KEY=IWbuods0f6fqWrHtAfm4hqBLuFAJ7m3rE7bigt6iuCk')
 ?>
