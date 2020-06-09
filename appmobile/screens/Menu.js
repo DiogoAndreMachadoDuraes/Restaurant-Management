6269a17d6d73235d7a2f241d1a12a8674c88a97e
AndreiaSousa <a37525@alunos.ipb.pt>
2020-06-09 20:21:01 +0100

    app mobile

--- appmobile/screens/Menu.js
+++ appmobile/screens/Menu.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { StyleSheet, Text, View, ScrollView, Button, ImageBackground, StatusBar, Image, TouchableOpacity } from "react-native";
 import {Header, Icon} from "react-native-elements";
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
-import NossoFinal from './NossoFinal.js';
+import NossoFinal from './shared/NossoFinal.js';
 
 const imageBackgound = { uri: "https://i.pinimg.com/originals/c8/cf/cb/c8cfcba6a515d39053198fd85fc79931.jpg" };
 
