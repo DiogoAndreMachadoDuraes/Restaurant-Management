bf22ba0396d65d6f0a65d5c7ad1082faaf68d6b3
AndreiaSousa <a37525@alunos.ipb.pt>
2020-06-16 17:16:34 +0100

    modificaçao no codigo Restaurante e Index

--- routes/index.php
+++ routes/index.php
@@ -52,6 +52,16 @@
     $app->post('/RefreshToken', Auth_controller::class . ':refreshToken');
 */
     $app->get('/Reserva', Reserva_controller::class . ':Select');
+    $app->get('/Administrador',Administrador_controller::class . ':Select');
+    $app->get('/Alergenio',Alergenio_controller::class . ':Select');
+    $app->get('/Fatura',Fatura_controller::class . ':Select');
+    $app->get('/Funcionario',Funcionario_controller::class . ':Select');
+    $app->get('/Info_nutricional',Info_nutricional_controller::class . ':Select');
+    $app->get('/Menu',Menu_controller::class . ':Select');
+    $app->get('/Produto',Produto_controller::class . ':Select');
+    $app->get('/Restaurante',Restaurante_controller::class . ':Select');
+    $app->get('/Take_away',Take_away_controller::class . ':Select');
+    $app->get('/Utilizador',Utilizador_controller::class . ':Select');
 
 /*
     $app->group('/Clientelogin', function() use($app) {
@@ -76,7 +86,7 @@
         $app->get('/Menu',Menu_controller::class . 'Select');
         $app->get('/Produto',Produto_controller::class . 'Select');
         $app->get('/Restaurante',Restaurante_controller::class . 'Select');
-        $app->get('/Take_away',Take_away_controller::class . 'Select');
+        $app->get('/Take_away',Take_away_controller::class . ':Select');
 
     })  ->add(new Auth_middleware())
         ->add(Jwt_Auth());
@@ -117,50 +127,50 @@
         $app->put('/Reserva', Reserva_controller::class . ':Update');
         $app->delete('/Reserva', Reserva_controller::class . ':Delete');
 
-        $app->get('/Funcionario',Funcionario_controller::class . 'Select');
-        $app->post('/Funcionario',Funcionario_controller::class . 'Insert');
-        $app->put('/Funcionario',Funcionario_controller::class . 'Update');
-        $app->delete('/Funcionario',Funcionario_controller::class . 'Delete');
+        $app->get('/Funcionario',Funcionario_controller::class . ':Select');
+        $app->post('/Funcionario',Funcionario_controller::class . ':Insert');
+        $app->put('/Funcionario',Funcionario_controller::class . ':Update');
+        $app->delete('/Funcionario',Funcionario_controller::class . ':Delete');
 
-        $app->get('/Alergenio',Alergenio_controller::class . 'Select');
-        $app->post('/Alergenio',Alergenio_controller::class . 'Insert');
-        $app->put('/Alergenio',Alergenio_controller::class . 'Update');
-        $app->delete('/Alergenio',Alergenio_controller::class . 'Delete');
+        $app->get('/Alergenio',Alergenio_controller::class . ':Select');
+        $app->post('/Alergenio',Alergenio_controller::class . ':Insert');
+        $app->put('/Alergenio',Alergenio_controller::class . ':Update');
+        $app->delete('/Alergenio',Alergenio_controller::class . ':Delete');
 
-        $app->get('/Fatura',Fatura_controller::class . 'Select');
-        $app->post('/Fatura',Fatura_controller::class . 'Insert');
-        $app->put('/Fatura',Fatura_controller::class . 'Update');
-        $app->delete('/Fatura',Fatura_controller::class . 'Delete');
+        $app->get('/Fatura',Fatura_controller::class . ':Select');
+        $app->post('/Fatura',Fatura_controller::class . ':Insert');
+        $app->put('/Fatura',Fatura_controller::class . ':Update');
+        $app->delete('/Fatura',Fatura_controller::class . ':Delete');
 
-        $app->get('/Administrador',Administrador_controller::class . 'Select');
-        $app->post('/Administrador',Administrador_controller::class . 'Insert');
-        $app->put('/Administrador',Administrador_controller::class . 'Update');
-        $app->delete('/Administrador',Administrador_controller::class . 'Delete');
+        $app->get('/Administrador',Administrador_controller::class . ':Select');
+        $app->post('/Administrador',Administrador_controller::class . ':Insert');
+        $app->put('/Administrador',Administrador_controller::class . ':Update');
+        $app->delete('/Administrador',Administrador_controller::class . ':Delete');
 
-        $app->get('/Info_nutricional',Info_nutricional_controller::class . 'Select');
-        $app->post('/Info_nutricional',Info_nutricional_controller::class . 'Insert');
-        $app->put('/Info_nutricional',Info_nutricional_controller::class . 'Update');
-        $app->delete('/Info_nutricional',Info_nutricional_controller::class . 'Delete');
+        $app->get('/Info_nutricional',Info_nutricional_controller::class . ':Select');
+        $app->post('/Info_nutricional',Info_nutricional_controller::class . ':Insert');
+        $app->put('/Info_nutricional',Info_nutricional_controller::class . ':Update');
+        $app->delete('/Info_nutricional',Info_nutricional_controller::class . ':Delete');
 
-        $app->get('/Menu',Menu_controller::class . 'Select');
-        $app->post('/Menu',Menu_controller::class . 'Insert');
-        $app->put('/Menu',Menu_controller::class . 'Update');
-        $app->delete('/Menu',Menu_controller::class . 'Delete');
+        $app->get('/Menu',Menu_controller::class . ':Select');
+        $app->post('/Menu',Menu_controller::class . ':Insert');
+        $app->put('/Menu',Menu_controller::class . ':Update');
+        $app->delete('/Menu',Menu_controller::class . ':Delete');
 
-        $app->get('/Produto',Produto_controller::class . 'Select');
-        $app->post('/Produto',Produto_controller::class . 'Insert');
-        $app->put('/Produto',Produto_controller::class . 'Update');
-        $app->delete('/Produto',Produto_controller::class . 'Delete');
+        $app->get('/Produto',Produto_controller::class . ':Select');
+        $app->post('/Produto',Produto_controller::class . ':Insert');
+        $app->put('/Produto',Produto_controller::class . ':Update');
+        $app->delete('/Produto',Produto_controller::class . ':Delete');
 
-        $app->get('/Restaurante',Restaurante_controller::class . 'Select');
-        $app->post('/Restaurante',Restaurante_controller::class . 'Insert');
-        $app->put('/Restaurante',Restaurante_controller::class . 'Update');
-        $app->delete('/Restaurante',Restaurante_controller::class . 'Delete');
+        $app->get('/Restaurante',Restaurante_controller::class . ':Select');
+        $app->post('/Restaurante',Restaurante_controller::class . ':Insert');
+        $app->put('/Restaurante',Restaurante_controller::class . ':Update');
+        $app->delete('/Restaurante',Restaurante_controller::class . ':Delete');
 
-        $app->get('/Take_away',Take_away_controller::class . 'Select');
-        $app->post('/Take_away',Take_away_controller::class . 'Insert');
-        $app->put('/Take_away',Take_away_controller::class . 'Update');
-        $app->delete('/Take_away',Take_away_controller::class . 'Delete');
+        $app->get('/Take_away',Take_away_controller::class . ':Select');
+        $app->post('/Take_away',Take_away_controller::class . ':Insert');
+        $app->put('/Take_away',Take_away_controller::class . ':Update');
+        $app->delete('/Take_away',Take_away_controller::class . ':Delete');
 
         $app->post('/Token',Token_dao::class . 'Insert');
 
