fe3bd0757c2e24fd9ebaebaefdf94dc4c8581248
Diogo Durães <61517303+DiogoAndreMachadoDuraes@users.noreply.github.com>
2020-06-22 15:32:30 +0100

    No cliente estão as excecoes e no conncection adicionei o utf8

--- app/dao/ConnectionDB.php
+++ app/dao/ConnectionDB.php
@@ -1,7 +1,9 @@
 <?php
 
-   namespace App\DAO;
-   
+namespace App\DAO;
+
+   use PDOException;
+
    abstract class ConnectionDB
    {
       protected $pdo;
@@ -15,13 +17,16 @@
          $port=getenv('ementarestauracao_PORT=');
          
          $dsn="mysql:host={$host};dbname={$dbname};port={$port}";*/
-         $dsn="mysql:host=178.62.212.71;dbname=ementarestauracao;port=3306";
-
-         $this->pdo= new \PDO($dsn, 'rh_user', 'rFrH2016');
-         $this->pdo->setAttribute(
-            \PDO::ATTR_ERRMODE,
-            \PDO::ERRMODE_EXCEPTION
-         );
+         try{
+            $dsn="mysql:host=178.62.212.71;dbname=ementarestauracao;port=3306";
+            $this->pdo= new \PDO($dsn, 'rh_user', 'rFrH2016', array(\PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $this->pdo->setAttribute(
+               \PDO::ATTR_ERRMODE,
+               \PDO::ERRMODE_EXCEPTION
+            );
+         } catch (PDOException $e){
+            die("Não foi possível a conexão com a base de dados... Contacte o administrador!");
+         }
       }
    }
 ?>
--- app/models/Cliente.php
+++ app/models/Cliente.php
@@ -16,6 +16,9 @@ namespace App\Models;
         }
 
         public function set_id_cliente(int $id_cliente): Cliente{
+            if (!$id_cliente&& !is_int($id_cliente)) {
+                throw new \InvalidArgumentException("É preciso o id de cliente", 400);
+            }
             $this->id_cliente = $id_cliente;
             return $this;
         }
@@ -26,6 +29,9 @@ namespace App\Models;
         }
         
         public function set_numero_cartao(int $numero_cartao): Cliente{
+            if (!$numero_cartao && !is_int($numero_cartao)) {
+                throw new \InvalidArgumentException("É preciso o número do cartão", 400);
+            }
             $this->numero_cartao = $numero_cartao;
             return $this;
         }
@@ -35,6 +41,12 @@ namespace App\Models;
         }
 
         public function set_n_compras(int $n_compras): Cliente{
+            if (!$n_compras) {
+                throw new \InvalidArgumentException("É preciso o número de compras", 400);
+            }
+            if (!is_int($n_compras)) {
+                throw new \InvalidArgumentException("Introduziu um valor para o número de compras incorreto", 400);
+            }
             $this->n_compras = $n_compras;
             return $this;
         }
@@ -44,6 +56,9 @@ namespace App\Models;
         }
 
         public function set_id_utilizador(int $id_utilizador): Cliente{
+            if (!$id_utilizador && !is_int($id_utilizador)) {
+                throw new \InvalidArgumentException("É preciso o id de utilizador correspondente", 400);
+            }
             $this->id_utilizador = $id_utilizador;
             return $this;
         }
