6c931db65876503a8d08a0c0cf5d8414f1b0d690
AndreiaSousa <a37525@alunos.ipb.pt>
2020-08-06 22:40:52 +0100

    ecra takeaway com introduçao ao animatable e collapsible, ainda em desenvolvimento

--- appmobile/screens/TakeAway.js
+++ appmobile/screens/TakeAway.js
@@ -1,44 +1,140 @@
 import * as React from "react";
-import { StyleSheet, Text, View, ScrollView, ImageBackground } from "react-native";
+import { StyleSheet, FlatList, Text, View, ScrollView, ImageBackground, TouchableOpacity } from "react-native";
 import {Header, Icon} from "react-native-elements";
 import { HeaderWihoutShop } from './shared/HeaderWihoutShop.js';
+import Icon2 from "react-native-vector-icons/MaterialCommunityIcons";
+import Accordion from 'react-native-collapsible/Accordion';
+import * as Animatable from 'react-native-animatable';
+import Collapsible from 'react-native-collapsible';
+
 
 const imageBackgound = { uri: "https://i.pinimg.com/originals/c8/cf/cb/c8cfcba6a515d39053198fd85fc79931.jpg" };
+
+const SECTIONS = [
+  {
+    title: 'Data Marcada',
+    content: '10/09/2020',
+  },
+  {
+    title: 'Hora Maracada',
+    content: '12h:30min',
+  },
+  {
+    title: 'Número de Pessoas',
+    content: '7 pessoas',
+  },
+  {
+    title: 'Nome da Resrva',
+    content: 'José Leite',
+  },
+  {
+    title: 'Valor Total',
+    content: '€21.40',
+  },
+  {
+    title: 'Estado da encomenda',
+    content: 'Em preparação ',
+  },
+];
 
 class TakeAway extends React.Component{
     constructor(){
         super();
         this.state={
           name:"Take Away",
+          activeSections: [],
         };
       }
       componentDidMount(){ 
         console.log("Mounting the screen TakeAway...");
       }
+      
+    
+      _renderSectionTitle = section => {
+        return (
+          <View style={style.content}>
+            <Text>{section.content}</Text>
+          </View>
+        );
+      };
+    
+      _renderHeader = section => {
+        return (
+          <View style={style.header}>
+            <Text style={style.headerText}>{section.title}</Text>
+          </View>
+        );
+      };
+    
+      _renderContent = section => {
+        return (
+          <View style={style.content}>
+            <Text>{section.content}</Text>
+          </View>
+        );
+      };
+    
+      _updateSections = activeSections => {
+        this.setState({ activeSections });
+      };
+
+      renderHeader(section, index, isActive, sections) {
+        return (
+          <Animatable.View
+            duration={300}
+            transition="backgroundColor"
+            style={{ backgroundColor: (isActive ? 'black' : 'black') }}>
+            <Text style={style.headerText}>{section.title}</Text>
+          </Animatable.View>
+        );
+      }
+    
+      _renderContent(section, i, isActive, sections) {
+        return (
+          <Animatable.View
+            duration={300}
+            transition="backgroundColor"
+            style={{ backgroundColor: (isActive ? 'black' : 'black') }}>
+            <Animatable.Text
+              duration={300}
+              easing="ease-out"
+              animation={isActive ? 'zoomIn' : false}>
+              {section.content}
+            </Animatable.Text>
+          </Animatable.View>
+        );
+      }
+
       render(){
         return (
           <View style={style.container}>
           <ImageBackground style={style.imageBackground} opacity={0.4}>
-
-            <ScrollView>
-            <View style={style.menu}>
-            <HeaderWihoutShop nome={this.state.name} navigation={this.props.navigation}/>
-
-              <View style={style.menuText}>
-                <ImageBackground source={require('../assets/take.jpg')} style={style.imageBackgound} opacity={0.8}>             
-                <View>
-                <Text style={style.text}>Data marcada : José Leite Machado</Text> 
-                <Text style={style.text}>Hora marcada : José Leite Machado</Text> 
-                <Text style={style.text}>Quantidade de pessoas : José Leite Machado</Text> 
-                <Text style={style.text}>Nome da reserva : José Leite Machado</Text> 
-                <Text style={style.text}>Valor total : José Leite Machado</Text> 
-                <Text style={style.text}>Estado da reserva : José Leite Machado</Text> 
-                </View>
-                </ImageBackground>
-              </View>
-              </View>
+          <View style={style.containerCollapsible}>
+          <ScrollView>
+          <View style={style.menu}>
+          <HeaderWihoutShop nome={this.state.name} navigation={this.props.navigation}/>  
+          <Collapsible collapsed={this.state.collapsed} align="center">
+            <View style={style.content}>
+              <Text>
+                Bacon ipsum dolor amet chuck turducken landjaeger tongue spare
+                ribs
+              </Text>
+            </View>
+          </Collapsible>
+            <Accordion
+              sections={SECTIONS}
+              activeSections={this.state.activeSections}
+              renderSectionTitle={this._renderSectionTitle}
+              renderHeader={this._renderHeader}
+              renderContent={this._renderContent}
+              onChange={this._updateSections}
+            />
+            
+            
+            </View>
             </ScrollView>
-            </ImageBackground>
+          </View>
+          </ImageBackground>
           </View>
         );
       }
@@ -49,8 +145,15 @@ class TakeAway extends React.Component{
       },
 
       imageBackground: {
-        flex: 1,
-        color: "white",
+        flex: 1
+      },
+
+      imageBackgound: {                         //foto por tras do titulo
+        width: 395,
+        height: 200,
+        marginLeft: 0,
+        top: 200,
+        opacity: 1,
       },
 
       menu: {                           //scrollview
@@ -58,21 +161,33 @@ class TakeAway extends React.Component{
         height: 1690,
       },
 
-      imageBackgound: {                         //foto por tras do titulo
-        width: 395,
-        height: 200,
-        marginLeft: 0,
-        top: 0,
-        opacity: 1,
+      containerCollapsible: {
+        flex: 1,
+        backgroundColor: "black"
       },
 
-      text:{
-        fontSize: 15,
-        fontWeight: 'bold',
-        color: 'black',
-        marginLeft: 30,
-        top: 250,
-    }
-     
+      title: {
+        textAlign: 'center',
+        fontSize: 22,
+        fontWeight: '300',
+        marginBottom: 20,
+      },
+
+      header: {
+        backgroundColor: '#fff',         //boxtext with title
+        padding: 10,
+      },
+
+      headerText: {                              //titles
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: '500',
+      },
+
+      content: {                            //o que esta a verde
+        padding: 20,
+        backgroundColor: 'green',
+      }
+
     });
 export default TakeAway;
