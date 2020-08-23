f1aac2e0813cc7a3e35a3e2a644e95ca25359f0b
Diogo Durães <61517303+DiogoAndreMachadoDuraes@users.noreply.github.com>
2020-08-23 21:04:38 +0100

    Alteração no ListPages (adição das páginas todas)

--- frontend/src/components/shared/listPages/index.js
+++ frontend/src/components/shared/listPages/index.js
@@ -11,6 +11,17 @@ import LocationOnIcon from '@material-ui/icons/LocationOn';
 import PeopleIcon from '@material-ui/icons/People';
 import DescriptionIcon from '@material-ui/icons/Description';
 import BookIcon from '@material-ui/icons/Book';
+import DateRangeIcon from '@material-ui/icons/DateRange';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import AssignmentIndIcon from '@material-ui/icons/AssignmentInd';
+import EmojiPeopleIcon from '@material-ui/icons/EmojiPeople';
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
+import EmojiTransportationIcon from '@material-ui/icons/EmojiTransportation';
+import InfoIcon from '@material-ui/icons/Info';
+import AssignmentLateIcon from '@material-ui/icons/AssignmentLate';
+import LocalOfferIcon from '@material-ui/icons/LocalOffer';
+
 import { useHistory } from "react-router-dom";
 
 export function ListPages (){
@@ -32,6 +43,66 @@ export function ListPages (){
     history.push("/restaurant");
   }
 
+  function goUser() {
+    history.push("/user");
+  }
+
+  function goWeeklyMeal() {
+    history.push("/weeklymeal");
+  }
+
+  function goSpecialMenu() {
+    history.push("/specialmenu");
+  }
+
+  function goMenu() {
+    history.push("/menu");
+  }
+
+  function goExtra() {
+    history.push("/extra");
+  }
+
+  function goBuyMenu() {
+    history.push("/buymenu");
+  }
+
+  function goBuyProduct() {
+    history.push("/buyproduct");
+  }
+
+  function goInvoice() {
+    history.push("/invoice");
+  }
+
+  function goClient() {
+    history.push("/client");
+  }
+
+  function goAdmin() {
+    history.push("/admin");
+  }
+
+  function goAllergen() {
+    history.push("/allergen");
+  }
+
+  function goEmployee() {
+    history.push("/employee");
+  }
+
+  function goMenuProduct() {
+    history.push("/menuproduct");
+  }
+
+  function goNutricionalInformation() {
+    history.push("/nutricionalinformation");
+  }
+
+  function goTakeAway() {
+    history.push("/takeaway");
+  }
+
   return( 
     <div>
       <ListItem button>
@@ -48,27 +119,63 @@ export function ListPages (){
       </ListItem>
       <ListItem button>
         <ListItemIcon>
+          <DateRangeIcon />
+        </ListItemIcon>
+        <ListItemText primary="Refeição Semanal" onClick={goWeeklyMeal}/>
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <MenuBookIcon />
+        </ListItemIcon>
+        <ListItemText primary="Ementas" onClick={goSpecialMenu}/>
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
           <RestaurantMenuIcon />
         </ListItemIcon>
-        <ListItemText primary="Ementas"/>
+        <ListItemText primary="Menus" onClick={goMenu}/>
       </ListItem>
       <ListItem button>
         <ListItemIcon>
           <RestaurantIcon />
         </ListItemIcon>
-        <ListItemText primary="Menus"/>
+        <ListItemText primary="Produtos" onClick={goProduct}/>
       </ListItem>
       <ListItem button>
         <ListItemIcon>
           <FastfoodIcon />
         </ListItemIcon>
-        <ListItemText primary="Produtos" onClick={goProduct}/>
+        <ListItemText primary="Extras" onClick={goExtra}/>
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <LocalOfferIcon  />
+        </ListItemIcon>
+        <ListItemText primary="Produtos do Menu" onClick={goMenuProduct}/>
       </ListItem>
       <ListItem button>
         <ListItemIcon>
           <ShoppingCartIcon />
         </ListItemIcon>
-        <ListItemText primary="Carrinho"/>
+        <ListItemText primary="Compras Menu" onClick={goBuyMenu}/>
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <ShoppingBasketIcon />
+        </ListItemIcon>
+        <ListItemText primary="Compras Produto" onClick={goBuyProduct}/>
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <AssignmentLateIcon />
+        </ListItemIcon>
+        <ListItemText primary="Alergénio" onClick={goAllergen}/>
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <InfoIcon />
+        </ListItemIcon>
+        <ListItemText primary="Informação Nutricional" onClick={goNutricionalInformation}/>
       </ListItem>
       <ListItem button>
         <ListItemIcon>
@@ -78,15 +185,39 @@ export function ListPages (){
       </ListItem>
       <ListItem button>
         <ListItemIcon>
+          <EmojiTransportationIcon />
+        </ListItemIcon>
+        <ListItemText primary="Take Away" onClick={goTakeAway}/>
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
           <DescriptionIcon />
         </ListItemIcon>
-        <ListItemText primary="Faturas" />
+        <ListItemText primary="Faturas" onClick={goInvoice}/>
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <SupervisorAccountIcon />
+        </ListItemIcon>
+        <ListItemText primary="Administrador" onClick={goAdmin}/>
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <EmojiPeopleIcon />
+        </ListItemIcon>
+        <ListItemText primary="Funcionário" onClick={goEmployee}/>
+      </ListItem>
+      <ListItem button>
+        <ListItemIcon>
+          <AssignmentIndIcon />
+        </ListItemIcon>
+        <ListItemText primary="Cliente" onClick={goClient}/>
       </ListItem>
       <ListItem button>
         <ListItemIcon>
           <PeopleIcon />
         </ListItemIcon>
-        <ListItemText primary="Utilizadores" />
+        <ListItemText primary="Utilizadores" onClick={goUser}/>
       </ListItem>
     </div>
   );
